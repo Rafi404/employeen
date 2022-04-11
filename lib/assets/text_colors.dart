971e23ca5f7e7aa1b99120625e2c6385d1e81ac6
rafi404 <rafikmwayanad@gmail.com>
2022-04-11 12:21:35 +0530

    Removed files and folders

--- lib/assets/text_colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// Add all const colors and text here...
-Color textColorPrimary = const Color(0xFF222550);
-Color textColorSecondary = const Color(0xFF6D6D85);
-Color bgColor = const Color(0xFFFCFDFF);
